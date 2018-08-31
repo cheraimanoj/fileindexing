30f92be5631dc9a3084c5da549ee04bab174ce1f
DESKTOP-E3PJKQ0\Manav <cheraimanoj@gmail.com>
2018-08-31 22:47:20 +0530

    * and . matching with regex, more test cases input_*.txt

--- script.py
+++ script.py
@@ -2,30 +2,27 @@ import sys
 import copy
 
 #Adding space in [intending the line for +/-]
-def getSpace(prev_dot,listofline,dot_count,i,val, sp = 0):
+def getSpace(prev_dot,listofline,i,val, sp = 0):
     prev = prev_dot[0]
-    space_dots = listofline[prev].split()
-    space_dots[0] = ''.join([' '] * prev_dot[1]) + val
-    listofline[prev] = ' '.join(space_dots)
+    space_dots = (prev_dot[1]*" ") + val
+    listofline[prev]= listofline[prev].replace(prev_dot[1]*'.',space_dots,1)
 
     if sp:
         for j in range(prev + 1, i):
-            listofline[j] = ''.join([' '] * prev_dot[1]) + '  ' + listofline[j]
+            listofline[j] = ((prev_dot[1]+1)*" ") + listofline[j]
 
-#with open(sys.argv[1],'r') as filedata: 
-#    listofline = filedata.readlines()
 listofline=[]
 for line in sys.stdin:
     if(line != '\n'):
         listofline.append(line.strip())
 
+prev_dot = ()
 list_star = []
-list_dot=[]
 dot_star = []
 #checking for * and . in first charater postion
 for i,line in enumerate(listofline):
     if(line.startswith('*')):
-        len_stars = len(line.split()[0])
+        len_stars = (len(line) - len(line.lstrip('*')))
         #Fetchin first star
         if(not list_star):
             list_star.append(1)
@@ -38,27 +35,26 @@ for i,line in enumerate(listofline):
                 list_star=list_star[:len_stars-len(list_star)]
                 list_star[-1]+=1
 
-        each_line = line.split()
-        each_line[0] ='.'.join(str(x) for x in list_star)
-        listofline[i] = ' '.join(each_line)
-        
+        each_line = '.'.join(str(x) for x in list_star)
+        listofline[i] = listofline[i].replace(len_stars*'*',each_line,1)
+
     if (line.startswith('.')):
-        dot_count=len(line.split()[0])
+        dot_count = (len(line) - len(line.lstrip('.')))
         #Fetchin first dot in first star
-        if(not dot_star):
+        if(not prev_dot):
             dot_star=copy.copy(list_star)
             prev_dot =(i,dot_count)
         elif(dot_star==list_star):
             if(prev_dot[1] < dot_count):
-                getSpace(prev_dot,listofline,dot_count,i,'+', 1)
+                getSpace(prev_dot,listofline,i,'+', 1)
                 prev_dot = (i, dot_count)
             else:
-                getSpace(prev_dot, listofline, dot_count, i,'-', 1)
+                getSpace(prev_dot, listofline, i,'-', 1)
                 prev_dot = (i, dot_count)
         else:
             dot_star = copy.copy(list_star)
-            getSpace(prev_dot, listofline, dot_count, i,'-')
+            getSpace(prev_dot, listofline, i,'-')
             prev_dot = (i, dot_count)
-
-getSpace(prev_dot,listofline,dot_count,i,'-')
+if(prev_dot):
+	getSpace(prev_dot,listofline,i,'-')
 print('\n'.join(listofline))
